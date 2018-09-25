2792ab5de87a1bd0f3b8eef25ca636913b9d04de
Johannes <j.p.de.lange88@gmail.com>
2018-09-25 21:32:59 +0200

    fixed connection string things, IP grab not working. added method that checks for existing  and adds  new client data

--- AGS.Mobile/AGS.Mobile/UtilDAL.cs
+++ AGS.Mobile/AGS.Mobile/UtilDAL.cs
@@ -1,7 +1,12 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,19 +14,22 @@ namespace AGS.Mobile
 {
     public static class UtilDal
     {
+        public static string Route(string controller)
+        {
+            const string ip = @"196.252.75.110";
+            const string port = "49805";
+            var route = $@"/AGSoft/{controller}/";
+            return @"http://" + ip + ":" + port + route;
+        }
+
         private static HttpClient client = new HttpClient();
 
         public static string GetSurvey(string id)
         {
-            const string ipPort = @"http://192.168.0.9:49805";
-            const string getRoute = @"/AGsoft/Module/";
             var content = string.Empty;
-            var response = client.GetAsync(ipPort + getRoute + $"{id}").Result;
-
+            var response = client.GetAsync(Route("Module") + $"{id}").Result;
             if (response.IsSuccessStatusCode)
             {
-                // This is where i can check if i have an error with connection issues again
-
                 content = response.Content.ReadAsStringAsync().Result;
             }
             return content;
@@ -30,7 +38,7 @@ namespace AGS.Mobile
         public static string GetSurvey()
         {
             var content = string.Empty;
-            var response = client.GetAsync(@"http://192.168.0.9:49805/AGsoft/values").Result;
+            var response = client.GetAsync(Route("Module")).Result;
 
             if (response.IsSuccessStatusCode)
             {
@@ -46,8 +54,43 @@ namespace AGS.Mobile
             //var postData = new StringContent(aData);
             const string medType = "application/json";
             var postData = new StringContent(JsonConvert.SerializeObject(aData), Encoding.UTF8, medType);
-            const string url = @"http://192.168.0.9:49805/AGsoft/values/";
-            var response = client.PostAsync(url, postData).Result;
+            var response = client.PostAsync(Route("Module"), postData).Result;
+        }
+
+        public static bool queryClient(List<string> lData)
+        {
+            var content = string.Empty;
+            var responseGet = client.GetAsync(Route("Patient") + $"{lData[2]}").Result;
+            
+            if (responseGet.IsSuccessStatusCode)
+            {
+                // This is where i can check if i have an error with connection issues again
+
+                content = JsonConvert.DeserializeObject<string>(responseGet.Content.ReadAsStringAsync().Result);
+            }
+
+            if (content == "New")
+            {
+                var Pdata = new CModel
+                {
+                    Said = lData[2],
+                    Name = lData[0],
+                    Surname = lData[1]
+                };
+                
+                var jsonOb = JsonConvert.SerializeObject(Pdata, Formatting.Indented);
+
+                const string medType = "application/json";
+                var postData = new StringContent(jsonOb, System.Text.Encoding.UTF8, medType);
+                var response = client.PostAsync(Route("Patient"), postData).Result;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            
         }
     }
 }
