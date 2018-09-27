c7e9560b0faa4cae4abf953655af6c1551f91a0f
Johannes <j.p.de.lange88@gmail.com>
2018-09-27 18:48:46 +0200

    added new class for conversion methods and removed legacy code

---
+++ AGS.Mobile/AGS.Mobile/ViewModel/ConversionHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AGS.Mobile.ViewModel
+{
+    internal class ConversionHelper
+    {
+        /// <summary>
+        /// Description:    Quick converter to condition data before POST
+        /// </summary>
+        /// <param name="tick">Boolean answer from SwitchCell</param>
+        /// <returns>string(true = "1", false = "0")</returns>
+        public static string Bool2Bin(bool tick)
+        {
+            return tick == true ? "1" : "0";
+        }
+    }
+}
